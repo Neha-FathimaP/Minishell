3c82c752de8329d8e8525e41169eb8559cbd089a
Neha Fathima P <nehapalayil@gmail.com>
2026-05-13 21:11:48 +0530

    Update minishell.c

--- minishell.c
+++ minishell.c
@@ -7,12 +7,7 @@ char prompt_name[20]="minishell$ ";
     char input_string[25];
 int main()
 {
-    /*
-       1.clear the screen
-       2.display prompt name
-       3.initialize input
-       4.read input using function call
-       */
+
     system("clear");
     extract_external_commands(external_commands);
     scan_input(prompt_name,input_string);
